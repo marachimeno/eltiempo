c31e87946c655b8cd9b9e89a70d676904ff119ec
Mara Chimeno <marachimeno@gmail.com>
2020-11-22 17:07:36 +0100

    Created executable file and added executable permissions; created CLI controller

--- lib/eltiempo/cli.rb
+++ lib/eltiempo/cli.rb
@@ -5,34 +5,6 @@ require_relative '../eltiempo'
 module Eltiempo
   class CLI
     def call
-      case ARGV[0]
-      when '-today'
-        raise NoCityError if ARGV[1].nil?
-
-        Eltiempo.today(ARGV[1])
-      when '-av_min'
-        raise NoCityError if ARGV[1].nil?
-
-        Eltiempo.av_min(ARGV[1])
-      when '-av_max'
-        raise NoCityError if ARGV[1].nil?
-
-        Eltiempo.av_max(ARGV[1])
-      when '-h'
-        help_menu
-      end
-    end
-
-    def help_menu
-      puts <<-EOF
-      Welcome to eltiempo!
-      Usage: eltiempo [option]
-        -today city          get today's weather for a city
-        -av_max city         get the average maximum temperature for the week in a city
-        -av_min city         get the average minimum temperature for the week in a city
-
-        NOTE: the city name can go without quotation marks
-      EOF
     end
   end
 end
