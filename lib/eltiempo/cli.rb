5ebdcb1ad09d799964123b9b269383c2c9eb8a3c
Mara Chimeno <marachimeno@gmail.com>
2020-11-22 17:58:54 +0100

    add main functionality in cli controller; used ARGV to manage options and arguments in command line

--- lib/eltiempo/cli.rb
+++ lib/eltiempo/cli.rb
@@ -3,8 +3,31 @@
 require_relative '../eltiempo'
 
 module Eltiempo
+  # CLI controller
   class CLI
     def call
+      case ARGV[0]
+      when '-today'
+        Eltiempo.today(ARGV[1])
+      when '-av_min'
+        Eltiempo.av_min(ARGV[1])
+      when '-av_max'
+        Eltiempo.av_max(ARGV[1])
+      when '-h'
+        help_menu
+      end
+    end
+
+    def help_menu
+      puts <<-DOC
+      Welcome to eltiempo!
+      Usage: eltiempo [option]
+        -today city          get today's weather for a city
+        -av_max city         get the average maximum temperature for the week in a city
+        -av_min city         get the average minimum temperature for the week in a city
+
+        NOTE: the city name can go without quotation marks
+      DOC
     end
   end
 end
